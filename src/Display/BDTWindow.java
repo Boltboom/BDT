1b8f53d783547134b04a5e27a71b2e6ca1a20544
thetandoori <rahul@Rahuls-MacBook-Pro.local>
2015-11-29 17:40:04 -0500

    Added RSSI
    prints ut rssi of device

--- src/Display/BDTWindow.java
+++ src/Display/BDTWindow.java
@@ -16,6 +16,9 @@ import java.awt.BorderLayout;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.border.TitledBorder;
+
+import com.intel.bluetooth.RemoteDeviceHelper;
+
 import javax.swing.JList;
 
 import java.awt.Color;
@@ -86,7 +89,7 @@ public class BDTWindow extends JFrame implements ActionListener {
 		connectiontimes= new ArrayList<Long>();
 		DataDevices= new ArrayList<DataRelation>();
 		copydiscoveredDevices= new ArrayList<RemoteDevice>();
-
+		
 		if(type) {
 			pane = new AdvancedWindow();
 			draw();
@@ -273,6 +276,13 @@ public class BDTWindow extends JFrame implements ActionListener {
 			copydiscoveredDevices.add(btDevice);
 			System.out.println(copydiscoveredDevices.size());
 			String name;
+			try {
+				int x= RemoteDeviceHelper.readRSSI(btDevice);
+				System.out.println("Connection Strength: "+x);
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
 			try
 			{
 				name = btDevice.getFriendlyName(false);
