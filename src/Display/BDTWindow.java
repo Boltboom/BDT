dfac3bc9c04c0f5bf259310ac996ca82130e927a
Boltboom <miesvan88@gmail.com>
2015-11-30 00:02:17 -0500

    First Attempt at Fix

--- src/Display/BDTWindow.java
+++ src/Display/BDTWindow.java
@@ -16,16 +16,7 @@ import java.awt.BorderLayout;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.border.TitledBorder;
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 
->>>>>>> parent of 5642c1a... Revert "Added Table functionality (BUGGY)"
-=======
-
-import com.intel.bluetooth.RemoteDeviceHelper;
-
->>>>>>> parent of 899b08e... Revert "Added RSSI"
 import javax.swing.JList;
 
 import java.awt.Color;
@@ -73,6 +64,8 @@ public class BDTWindow extends JFrame implements ActionListener {
 	protected JLabel deviceLabel;
 	protected JLabel AssignLabel;
 	protected boolean operational = true;
+	
+	protected ArrayList<DataRelation> DataDevices;
 
 	/*
 	 * Main function, starts new BDTWindow.
@@ -92,13 +85,9 @@ public class BDTWindow extends JFrame implements ActionListener {
 	public void setup(boolean type) {
 		discoveredDevices = new ArrayList<RemoteDevice>();
 		connectiontimes= new ArrayList<Long>();
-<<<<<<< HEAD
-
-=======
 		DataDevices= new ArrayList<DataRelation>();
 		copydiscoveredDevices= new ArrayList<RemoteDevice>();
 		
->>>>>>> parent of 899b08e... Revert "Added RSSI"
 		if(type) {
 			draw();
 			detect();
@@ -128,6 +117,7 @@ public class BDTWindow extends JFrame implements ActionListener {
 			lock = new Object();
 			user = LocalDevice.getLocalDevice();
 			agent = user.getDiscoveryAgent();
+			copydiscoveredDevices.clear();
 			agent.startInquiry(DiscoveryAgent.GIAC, new MyDiscoveryListener());
 			try {
 				synchronized(lock) {
@@ -139,13 +129,13 @@ public class BDTWindow extends JFrame implements ActionListener {
 			deviceLabel.setText("");
 			String label = "<html><body>" + deviceLabel.getText();
 			int i=0;
-
+			removeunfound();
 			for(RemoteDevice a : discoveredDevices) {
 				Date temp=new Date();
 				System.out.println(a.getFriendlyName(false)+ " Display results time: " + temp);
 
 
-				double len=((double)(temp.getTime())-(double)(connectiontimes.get(i)))/1000;
+				double len=((double)(temp.getTime())-(double)(DataDevices.get(i).startconnection))/1000;
 				label += a.getFriendlyName(false) + " {" + a.getBluetoothAddress() + ") Connection Length: "+len+" seconds <br>";
 				i++;
 			}
@@ -232,8 +222,6 @@ public class BDTWindow extends JFrame implements ActionListener {
 
 		}
 	}
-<<<<<<< HEAD
-=======
 	
 	/*
 	 * Writer
@@ -277,18 +265,31 @@ public class BDTWindow extends JFrame implements ActionListener {
 		}
 	}
 	// Checks if devices in discovered devices are still found.
->>>>>>> parent of 5642c1a... Revert "Added Table functionality (BUGGY)"
 	public void removeunfound()
 	{
-		for(int i =0; i<copydiscoveredDevices.size();i++)
+		System.out.println("GOTHERE");
+		System.out.println("The size of the copy is :"+ copydiscoveredDevices.size());
+		if(copydiscoveredDevices.isEmpty())
 		{
-			for(int j=0; j<discoveredDevices.size();j++)
-			{
-				if(discoveredDevices.get(j).equals(copydiscoveredDevices.get(i)))
-				{
-					break;
-				}
+			discoveredDevices.clear();
 
+			
+			stopallTimes();
+			//Clear connection times
+			connectiontimes.clear();
+		}
+			
+		else
+		{
+			for(int i =0; i<discoveredDevices.size();i++)
+			{
+				if(!copydiscoveredDevices.contains(discoveredDevices.get(i)))
+				{
+					discoveredDevices.remove(i);
+					connectiontimes.remove(i);
+					System.out.println("REMOVED");
+					
+				}
 			}
 		}
 	}
@@ -298,60 +299,49 @@ public class BDTWindow extends JFrame implements ActionListener {
 	public class MyDiscoveryListener implements DiscoveryListener {
 
 		public void deviceDiscovered(RemoteDevice btDevice, DeviceClass arg1) {
-<<<<<<< HEAD
-			copydiscoveredDevices= new ArrayList<RemoteDevice>();
-			String name;
-			try {
-=======
 			copydiscoveredDevices.add(btDevice);
 			System.out.println(copydiscoveredDevices.size());
-<<<<<<< HEAD
 			String name; /*
-=======
-			String name;
->>>>>>> parent of 899b08e... Revert "Added RSSI"
 			try {
 				int x= RemoteDeviceHelper.readRSSI(btDevice);
 				System.out.println("Connection Strength: "+x);
 			} catch (IOException e1) {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
-<<<<<<< HEAD
 			} */
-=======
-			}
->>>>>>> parent of 899b08e... Revert "Added RSSI"
 			try
 			{
->>>>>>> parent of 5642c1a... Revert "Added Table functionality (BUGGY)"
 				name = btDevice.getFriendlyName(false);
-			} catch (Exception e) {
+			} 
+			catch (Exception e) 
+			{
 				name = btDevice.getBluetoothAddress();
 			}
-
+			// Adding all discovered devices to a copy.
+		
 			System.out.println("Device that was found: " +name);
 			if(!discoveredDevices.contains(btDevice))
 			{
-				discoveredDevices.add(btDevice);
+				DataRelation Data = new DataRelation();
 				Date d=new Date();
-
+				
+				Data.name= name;
+				Data.id=btDevice.getBluetoothAddress();
+				Data.discoverable=true;
+				Data.startconnection=d.getTime();
+							
+				discoveredDevices.add(btDevice);
+				
+				// add data-relation object to ArrayList
+				DataDevices.add(Data);						
 				System.out.println("Device: "+name + " Time added: " + d);
 				connectiontimes.add(d.getTime());
 
 			}
 
-			// Removing devices that have dissconnected.
-			//
-
-
-
-
-
-
 		}
 
 		public void inquiryCompleted(int arg0) {
-			removeunfound();
 			synchronized(lock) {
 				lock.notify();
 			}
